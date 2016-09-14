5fe44a502d0aea01043adda2f2d1bfdd5238b0f6
PERRIER Aurélien <perrie_a@etna-alternance.net>
2016-09-14 14:49:51 +0200

    Suppression des keys Oauth2 Github

--- server/config/auth.js
+++ server/config/auth.js
@@ -12,11 +12,5 @@ module.exports = {
         'clientID'      : '662568050796-fdeb4pggmniuogu92mobdh08pjgfqsro.apps.googleusercontent.com',
         'clientSecret'  : 'gzG_OAcU4gefYESxH6vuE-tc',
         'callbackURL'   : 'http://localhost:3000/api/auth/google/callback'
-    },
-
-    'githubAuth' : {
-        'clientID'      : '26c6f014b1c3d58d2ef1',
-        'clientSecret'  : '822fe7c5cdf42e72b16920bcd528af64263b811e',
-        'callbackURL'   : 'http://localhost:3000/api/auth/github/callback'
     }
 };
