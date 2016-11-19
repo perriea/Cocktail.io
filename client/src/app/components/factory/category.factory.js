942fcf276be5387aaea0f72f5d073c8dae253cae
misseur <lhomme_m@etna-alternance.net>
2016-11-19 15:21:22 +0100

    Ajout d'une factory pour les category

---
+++ client/src/app/components/factory/category.factory.js
@@ -0,0 +1,62 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cocktail.io')
+    .factory('CategoryFactory', CategoryFactory);
+
+    function CategoryFactory() {
+        var category_tab = [
+            {
+                title:      '',
+                subtitle:   'Contenu',
+                subclass:   'content',
+                text:       'Lorem Ipsum - Vidéos',
+                link:       '#/generator'
+            },
+            {
+                title:      '',
+                subtitle:   'Comptes',
+                subclass:   'accounts',
+                text:       'Identifiants - Mots de passe',
+                link:       '#/generator/username'
+            },
+            {
+                title:      '',
+                subtitle:   'HTML',
+                subclass:   'html',
+                text:       'Rien pour l\'instant',
+                link:       '#/generator/password'
+            },
+            {
+                title:      '',
+                subtitle:   'Base de données',
+                subclass:   'databases',
+                text:       'Rien pour l\'instant',
+                link:       '#/generator'
+            },
+            {
+                title:      '',
+                subtitle:   'CSS & Animations',
+                subclass:   'css',
+                text:       'Rien pour l\'instant',
+                link:       '#/generator'
+            },
+            {
+                title:      '',
+                subtitle:   'Back',
+                subclass:   'back',
+                text:       'Rien pour l\'instant',
+                link:       '#/generator'
+            }
+        ];
+
+        function getCategory() {
+            return category_tab;
+        }
+
+        return {
+            getCategory: getCategory
+        };
+    }
+})();
--- client/src/app/components/navbar/navbar.directive.js
+++ client/src/app/components/navbar/navbar.directive.js
@@ -20,10 +20,42 @@
     return directive;
 
     /** @ngInject */
-    function NavbarController() {
+    function NavbarController($mdDialog) {
       var vm = this;
+      /*
+      vm.connection  = connection;
+      vm.inscription = inscription;
+
+      function connection() {
+
+      }
+
+      function inscription() {
+
+      }
+
+      function show() {
+        $mdDialog.show({
+          controller:          DialogController,
+          parent:              angular.element(document.body),
+          templateUrl:         'app/components/navbar/dialog.html',
+          clickOutsideToClose: true
+        }).then(callBackSuccess, callBackError);
+      }
+
+      function callBackSuccess(result) {
+        console.log(result);
+      }
+
+      function callBackError(result) {
+        console.log(result);
+      } */
 
     }
   }
+/*
+  function DialogController($mdDialog) {
+      var vm = this;
+  } */
 
 })();
