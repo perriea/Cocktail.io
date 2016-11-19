3a8266029a8548b347639cd1272eda20ffff6618
misseur <lhomme_m@etna-alternance.net>
2016-11-19 13:25:39 +0100

    input video

--- client/src/app/generator-lorem-video/video-generator/video.directive.js
+++ client/src/app/generator-lorem-video/video-generator/video.directive.js
@@ -20,18 +20,31 @@
     return directive;
 
     /** @ngInject */
-    function VideoController(Generator) {
-      var vm         = this;
-      vm.loading     = false;
-      vm.autoplay    = false;
-      vm.height      = 400;
-      vm.width       = 400;
-      vm.src         = '';
+    function VideoController(Generator, $location) {
+      var vm      = this;
+      vm.autoplay = false;
+      vm.loop     = false;
+      vm.controls = false;
+      vm.muted    = false;
+      vm.height   = 400;
+      vm.width    = 400;
+      vm.src      = '';
 
-      vm.sendRequest = sendRequest;
+      vm.intern  = false;
+      vm.loading = false;
+
+      console.log($location.path());
+
+      vm.intern_src = [
+        { name: "bloub.mp4", path: "/bloub.mp4"},
+        { name: "pouet.mp4", path: "/pouet.mp4"}
+      ];
+
+      vm.setInternVideo = setInternVideo;
+      vm.sendRequest    = sendRequest;
 
       function sendRequest() {
-        if ($scope.loading === true) {
+        if (vm.loading === true) {
           return ;
         }
         vm.loading = true;
@@ -39,21 +52,31 @@
 
         Generator
         .getVideo({
-          autoplay: 'vm.autoplay',
-          height:   'vm.height',
-          width:    'vm.width',
-          src:      'vm.src'
+          autoplay: vm.autoplay,
+          controls: vm.controls,
+          height:   vm.height,
+          width:    vm.width,
+          loop:     vm.loop,
+          muted:    vm.muted,
+          src:      vm.src
         }).$promise
         .then(callBackSuccess)
-        ["catch"](callsBackError)
+        ["catch"](callBackError)
         ["finally"](callBackFinally);
 
         /* $http.get("someurl/?caracters#{vm.caracters}&paragraphes=#{vm.paragraphes}")
         .then(callBackSuccess, callsBackError); */
       }
 
+      function setInternVideo(link) {
+        vm.intern  = true;
+        vm.display = link.name;
+        vm.src     = link.path;
+      }
+
       function callBackSuccess (result) {
         vm.result = result;
+        console.log(result);
       }
       function callBackError (error) {
         console.log(error);
