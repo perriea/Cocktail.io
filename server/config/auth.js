2ea1dc828ee28f5d47f7c43b9b458fd7ed03ed0d
Aurélien Perrier <a.perrier89@gmail.com>
2016-11-23 14:15:23 +0100

    Ajouts de commentaires + mise a zero du fichier de config oAuth

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,16 +1,15 @@
 // config/auth.js
 
-// expose our config directly to our application using module.exports
 module.exports = {
     'twitterAuth' : {
-        'consumerKey'       : 'blabla',
-        'consumerSecret'    : 'blabla',
-        'callbackURL'       : 'blabla'
+        'consumerKey'       : '',
+        'consumerSecret'    : '',
+        'callbackURL'       : ''
     },
 
     'googleAuth' : {
-        'clientID'      : '662568050796-o70r0o9a9h5407s711crcmdrou79arv0.apps.googleusercontent.com',
-        'clientSecret'  : 'g38kWCEDaPOUqpZhAL7Kmt5q',
-        'callbackURL'   : 'http://localhost:3000/api/auth/google/callback'
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
     }
 };
